d89e28a54ec8d5741ffe2c56461493bc5ea5f78c
David Rawlings <davidrawlings10@gmail.com>
2020-02-18 21:55:06 -0800

    commenting out query that was causing error

--- src/main/java/scoreboard/TeamRepository.java
+++ src/main/java/scoreboard/TeamRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface TeamRepository extends CrudRepository<Team, Integer> {
-    @Query("from Team t join t.league l where l.id=:leagueId")
-    public Iterable<Team> findByLeagueId(@Param("leagueId") int leagueId);
+    //@Query("select t from Team t join t.league l where l.id=:leagueId")
+    //public Iterable<Team> findByLeagueId(@Param("leagueId") int leagueId);
 }

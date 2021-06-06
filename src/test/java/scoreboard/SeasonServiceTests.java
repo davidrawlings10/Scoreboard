8950a80d8d897c9ae80db0e37f0acf2b4c0bf64f
David Rawlings <davidrawlings10@gmail.com>
2021-06-05 22:49:19 -0700

    adding unit test for schedule rounds

--- src/test/java/scoreboard/SeasonServiceTests.java
+++ src/test/java/scoreboard/SeasonServiceTests.java
@@ -1,5 +1,6 @@
 package scoreboard;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,12 +38,36 @@ public class SeasonServiceTests {
         assert(homeGameCountForTeam1 + awayGameCountForTeam1 == teamIds.size() * 3 / 2);
     }
 
+    @Test
+    public void testRounds() {
+        List<Integer> teamIds = Arrays.asList(1, 2, 3, 4);
+
+        final int numGames = 4;
+
+        List<Game> games = seasonService.scheduleSeasonRound(teamIds, numGames);
+
+        assert(games.size() == teamIds.size() * numGames / 2);
+
+        int homeGameCountForTeam1 = 0, awayGameCountForTeam1 = 0;
+        for (Game game : games) {
+            if (game.getHomeTeamId() == 1) {
+                homeGameCountForTeam1++;
+            } else if (game.getAwayTeamId() == 1) {
+                awayGameCountForTeam1++;
+            }
+        }
+
+        assert(homeGameCountForTeam1 == awayGameCountForTeam1);
+        assert(homeGameCountForTeam1 + awayGameCountForTeam1 == numGames);
+    }
+
     // tests with DB
     @Test
     public void testScheduleSeasonHomeRotation() throws Exception {
         seasonService.scheduleSeason(ScheduleType.HOME_ROTATION, Sport.HOCKEY, 3, 4);
     }
 
+    @Ignore
     @Test
     public void testScheduleSeasonRounds() throws Exception {
         seasonService.scheduleSeason(ScheduleType.ROUNDS, Sport.HOCKEY, 3, 4);
